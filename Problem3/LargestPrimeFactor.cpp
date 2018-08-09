90c776eecbc73763b5ed8f15e135a4cbecb7a8c2
6R347-0BL1V10N <39090606+6R347-0BL1V10N@users.noreply.github.com>
2018-08-09 09:55:36 -0400

    LargestPrimeFactor
    The prime factors of 13195 are 5, 7, 13 and 29.
    What is the largest prime factor of the number 600851475143 ?

---
+++ Problem3/LargestPrimeFactor.cpp
@@ -0,0 +1,8 @@
+/*
+//Author      : 6R347-0BL1V10N
+//date        : August 9, 2018
+//purpose     : ProjectEuler.net - Problem 3
+
+//description : The prime factors of 13195 are 5, 7, 13 and 29.
+				What is the largest prime factor of the number 600851475143 ?
+*/
